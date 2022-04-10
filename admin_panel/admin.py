76470b93f6ba61cd1d1440612838f9787f9bc5f6
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-04-10 22:42:42 +0600

    product model created

--- admin_panel/admin.py
+++ admin_panel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'selling_price', 'discount_price', 'descriptions', 'brand', 'category', 'product_image']
--- admin_panel/models.py
+++ admin_panel/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
+CATEGORY_CHOICES = (
+    ('S', 'Shirts'),
+    ('P', 'Pants'),
+    ('W', 'Watch'),
+    ('T', 'Tie'),
+    ('SH', 'Shoes'),
+)
 
 
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    selling_price = models.FloatField()
+    discount_price = models.FloatField()
+    descriptions = models.CharField(max_length=200)
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=10)
+    product_image = models.ImageField(upload_to='product_image')
 
-
+    def __str__(self):
+        return str(self.id)
