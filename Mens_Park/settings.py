9e00009b11f1d429be01172c058e67e40cd1c2fd
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-04-10 18:03:44 +0600

    home page is  created

--- Mens_Park/settings.py
+++ Mens_Park/settings.py
@@ -15,6 +15,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin_panel',
+    'customer_panel',
 ]
 
 MIDDLEWARE = [
@@ -82,5 +84,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
