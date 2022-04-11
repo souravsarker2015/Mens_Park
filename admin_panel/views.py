e83e04e71d981f00800a1dc976e13b89dbe5186d
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-04-11 10:13:22 +0600

    products are showing in the home page

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .models import Product, Outlet
+
 
 
 def home(request):
-    return render(request, 'admin_panel/home.html')
+    shirts = Product.objects.filter(category='S')
+    pants = Product.objects.filter(category='P')
+    watches = Product.objects.filter(category='W')
+    ties = Product.objects.filter(category='T')
+    shoes = Product.objects.filter(category='SH')
+
+    context = {
+        'shirts': shirts,
+        'pants': pants,
+        'watches': watches,
+        'ties': ties,
+        'shoes': shoes,
+    }
+
+    return render(request, 'admin_panel/home.html',context)
+
+
