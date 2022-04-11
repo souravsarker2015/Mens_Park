bb3baeb1ceffd7b0b2ae40b37845d184197ee42b
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-04-11 11:57:17 +0600

    product details are showing and product dropdown created also working ordering

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('product-detail/<int:pk>', views.product_details, name="product-detail"),
+    path('shirt/', views.shirts, name='shirt'),
+    path('shirt_details/<slug:data>', views.shirts, name='shirt_details'),
+    path('pant/', views.pants, name='pant'),
+    path('pant_details/<slug:data>', views.pants, name='pant_details'),
+    path('watch/', views.watches, name='watch'),
+    path('watch_details/<slug:data>', views.watches, name='watch_details'),
+    path('tie/', views.ties, name='tie'),
+    path('tie_details/<slug:data>', views.ties, name='tie_details'),
+    path('shoe/', views.shoes, name='shoe'),
+    path('shoe_details/<slug:data>', views.shoes, name='shoe_details'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import Product, Outlet
 
 
-
 def home(request):
     shirts = Product.objects.filter(category='S')
     pants = Product.objects.filter(category='P')
@@ -17,7 +16,72 @@ def home(request):
         'ties': ties,
         'shoes': shoes,
     }
+    return render(request, 'admin_panel/home.html', context)
 
-    return render(request, 'admin_panel/home.html',context)
+
+def product_details(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {
+        'product': product,
+    }
+    return render(request, 'admin_panel/productdetail.html', context)
 
 
+def shirts(request, data=None):
+    if data is None:
+        shirt = Product.objects.filter(category='S')
+    elif data == 'below':
+        shirt = Product.objects.filter(category='S').filter(discount_price__lt=1000)
+
+    context = {
+        'shirt': shirt,
+    }
+    return render(request, 'admin_panel/shirts.html', context)
+
+
+def pants(request, data=None):
+    if data is None:
+        pant = Product.objects.filter(category='P')
+    elif data == 'below':
+        pant = Product.objects.filter(category='P').filter(discount_price__lt=1500)
+
+    context = {
+        'pant': pant,
+    }
+    return render(request, 'admin_panel/pants.html', context)
+
+
+def watches(request, data=None):
+    if data is None:
+        watch = Product.objects.filter(category='W')
+    elif data == 'below':
+        watch = Product.objects.filter(category='W').filter(discount_price__lt=20000)
+
+    context = {
+        'watch': watch,
+    }
+    return render(request, 'admin_panel/watch.html', context)
+
+
+def ties(request, data=None):
+    if data is None:
+        tie = Product.objects.filter(category='T')
+    elif data == 'below':
+        tie = Product.objects.filter(category='T').filter(discount_price__lt=800)
+
+    context = {
+        'tie': tie,
+    }
+    return render(request, 'admin_panel/tie.html', context)
+
+
+def shoes(request, data=None):
+    if data is None:
+        shoe = Product.objects.filter(category='SH')
+    elif data == 'below':
+        shoe = Product.objects.filter(category='SH').filter(discount_price__lt=18000)
+
+    context = {
+        'shoe': shoe,
+    }
+    return render(request, 'admin_panel/shoes.html', context)
